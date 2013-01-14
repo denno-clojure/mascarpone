6cd065c28621135394fd7bb81615cd23d41d83db
Nicolas Modrzyk <hellonico@gmail.com>
2013-01-14 20:48:12 +0900

    chapter 02

---
+++ chapter02/project.clj
@@ -0,0 +1,15 @@
+(defproject chapter02 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+    ; clojure
+    [org.clojure/clojure "1.5.0-alpha5"]
+
+  	; colors in your REPL
+  	[colorize "0.1.1"]
+
+  	; http kit, websockets
+  	[me.shenfeng/http-kit "1.3-SNAPSHOT"]
+  ])
---
+++ chapter02/test/chapter02/core_test.clj
@@ -0,0 +1,7 @@
+(ns chapter02.core-test
+  (:use clojure.test
+        chapter02.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
