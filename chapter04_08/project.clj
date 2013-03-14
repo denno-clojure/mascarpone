bed24d365463815689c8a9a95dcaed6558feedb6
Nicolas Modrzyk <nico@fivecool.net>
2013-03-14 20:12:12 +0900

    liberator

--- chapter04_08/project.clj
+++ chapter04_08/project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  
+  :plugins [[lein-ring "0.8.3"]]
+  :ring {:handler chapter04-08.core/app}
   :dependencies [
     [compojure "1.0.2"]
-	[ring/ring-jetty-adapter "1.1.0"]
+	  [ring/ring-jetty-adapter "1.1.0"]
     [liberator "0.8.0"]
   	[org.clojure/clojure "1.4.0"]])
--- chapter04_08/src/chapter04_08/core.clj
+++ chapter04_08/src/chapter04_08/core.clj
@@ -1,6 +1,16 @@
-(ns chapter04-08.core)
+(ns chapter04-08.core
+ (:use [liberator.core])
+ (:use [compojure.core]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def postbox-counter (atom 0))
+
+(defresource postbox
+  :available-media-types ["text/html" "text/plain"]
+  :method-allowed? (request-method-in :post :get)
+  :post! (fn [_] (swap! postbox-counter inc))
+  :handle-created (fn [_] (str "Your submission was accepted. The counter is now " @postbox-counter))
+  :handle-ok (fn [_] (str "The counter is " @postbox-counter)))
+
+(defroutes app
+  (ANY "/" [] (resource :available-media-types ["text/plain"] :handle-ok "Hello World!"))
+  (ANY "/post" [] postbox))
