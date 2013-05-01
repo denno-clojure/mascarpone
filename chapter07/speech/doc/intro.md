cd1f72300b4822534cb3fc2a45d91a564145e23a
Nicolas Modrzyk <hellonico@gmail.com>
2013-05-01 20:34:23 +0900

    so much fun

---
+++ chapter07/speech/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to speech
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ chapter07/speech/project.clj
@@ -0,0 +1,22 @@
+(defproject speech "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+	[facts/speech-recognition "1.0.0"]
+	[facts/speech-synthesis "1.0.0"]
+	[org.clojure/clojure "1.5.1"]])
+
+; fix recognition
+; https://github.com/klutometis/speech-recognition/blob/master/src/speech_recognition/hear.clj
+; http://docs.oracle.com/javase/1.5.0/docs/api/javax/sound/sampled/AudioSystem.html
+
+; synthesis works so great
+; https://github.com/klutometis/speech-synthesis
+
+; siri clone
+; https://github.com/klutometis/iris
+
+; this guy is awesome
+; https://github.com/klutometis?tab=repositories
