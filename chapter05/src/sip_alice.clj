60097bdfc83bd24b8f8c676cbcc819675ed24056
Nicolas Modrzyk <nico@fivecool.net>
2013-08-28 17:02:44 +0900

    format

--- chapter05/src/sip_alice.clj
+++ chapter05/src/sip_alice.clj
@@ -5,19 +5,33 @@
 
 (defmethod handle-request :MESSAGE [request & [transaction]]
   (println "Received: " (String. (.getContent request)))
-  (send-response! 200 :in transaction :pack "I am Alice and I received a message."))
+  (send-response! 
+  	200 
+  	:in transaction
+  	:pack "I am Alice and I received a message."))
 
-(global-set-account :user "alice" :domain "localhost" :display-name "Alice" :password "123456")
+(global-set-account 
+	:user "alice" 
+	:domain "localhost" 
+	:display-name "Alice" 
+	:password "123456")
 
-(sip/global-bind-sip-provider! (sip/sip-provider! "my-app" "localhost" 5061 "udp"))
+(sip/global-bind-sip-provider! 
+	(sip/sip-provider! "my-app" "localhost" 5061 "udp"))
 (sip/set-listener! (dum-listener))
 (sip/start!)
 
 (defn send-message[target msg]
   (send-request! :MESSAGE :to (addr/address target) :pack msg
-    :on-success (fn [& {:keys [response]}] (println "Fine! response: " (.getContent response)))
-    :on-failure (fn [& {:keys [response]}] (println "Oops!" (.getStatusCode response)))
-    :on-timeout (fn [_] (println "Timeout, try it later.")))
+    :on-success 
+    　　(fn [& {:keys [response]}] 
+    	　　(println "Fine! response: " (.getContent response)))
+    :on-failure 
+    　　(fn [& {:keys [response]}] 
+    	　　(println "Oops!" (.getStatusCode response)))
+    :on-timeout 
+    　　(fn [_] 
+    	　　(println "Timeout, try it later.")))
   )
 
 (send-message "sip:bob@localhost" "Hello, Bob.")
