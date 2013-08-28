19a6673766373b0c16b9a7447bde2e8e311a1ff8
Nicolas Modrzyk <nico@fivecool.net>
2013-08-28 17:06:15 +0900

    format

--- chapter05_hadoop/hbase_query/src/query.clj
+++ chapter05_hadoop/hbase_query/src/query.clj
@@ -3,8 +3,13 @@
 
 ; table-name key-field column-family & value-fields
 (let [table (hbase-tap "test-users" "testrow" "test-cf-name" "test")]
-  (?<- 								; compose and execute a query: (?<- out-tap out-vars & predicates)
-  	(stdout) 						; output to stdout
-  	[?id ?c] 						; two values to compute
-  	(table ?id ?test-cf-name) 		; compute ?id, the key of the row
-  	(slurp ?test-cf-name :> ?c))) 	; compute ?c. returned as bytes
+  ; compose and execute a query: (?<- out-tap out-vars & predicates)
+  (?<- 								
+    ; output to stdout
+  	(stdout) 						
+  	; two values to compute
+  	[?id ?c] 						
+  	; compute ?id, the key of the row
+  	(table ?id ?test-cf-name) 		
+  	; compute ?c. returned as bytes
+  	(slurp ?test-cf-name :> ?c))) 	
