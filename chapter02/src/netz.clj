0031a6f2bde0da78b805f50fc9ba1131ccbea89e
Nicolas Modrzyk <hellonico@gmail.com>
2013-08-29 10:00:07 +0900

    format

--- chapter02/src/netz.clj
+++ chapter02/src/netz.clj
@@ -9,7 +9,11 @@
 ; takes some time to train the network
 (def network (netz/train examples {:hidden-neurons [1]}))
 
-(netz/run network [0 0]) ; => [0.9176]
-(netz/run network [0 1]) ; => [0.0549]
-(netz/run network [1 0]) ; => [0.0728]
-(netz/run network [1 1]) ; => [0.9307]
+(netz/run network [0 0]) 
+; => [0.9176]
+(netz/run network [0 1]) 
+; => [0.0549]
+(netz/run network [1 0]) 
+; => [0.0728]
+(netz/run network [1 1]) 
+; => [0.9307]
